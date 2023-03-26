9f6326858d7e68f7b13ed913a8b0add68120a13d
RIPO12345 <placida.ripo.naibaho@gmail.com>
2023-03-26 16:33:00 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302213103/Program.cs
+++ tpmodul6_1302213103/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_1302213103;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // membuat video baru dengan judul "Tutorial Design By Contract – [NAMA_PRAKTIKAN]"
+        string videoTitle = "Tutorial Design By Contract - [Placida Ripo Naibaho]";
+        SayaTubeVideo myVideo = new SayaTubeVideo(videoTitle);
+
+        // call IncreasePlayCount method to add 100 views
+        myVideo.IncreasePlayCount(100);
+
+        // call PrintVideoDetails method to print video details
+        myVideo.PrintVideoDetails();
+
+        Console.ReadLine(); // keep console window open
+    }
+}
---
+++ tpmodul6_1302213103/SayaTubeVideo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302213103
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string videoTitle)
+        {
+            // generate random id with 5 digits
+            Random rand = new Random();
+            id = rand.Next(10000, 99999);
+
+            title = videoTitle;
+            playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("======== Video Details ========");        
+            Console.WriteLine("ID           : " + id);
+            Console.WriteLine("Title        : " + title);
+            Console.WriteLine("Play Count   : " + playCount);
+        }
+    }
+}
